c1c20b74600e3c2b8fe27c02437ef23e3cab4d15
Mohammad Hasnain <mformohammadhasnain@gmail.com>
2025-05-29 14:33:06 +0500

    Add db pool connection

--- src/config/db.js
+++ src/config/db.js
@@ -1,19 +1,14 @@
 const mysql = require('mysql2');
 
-const db = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT, // Add port from .env
+  port: process.env.DB_PORT, // Use port from .env
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-db.connect((err) => {
-  if (err) {
-    console.error('Database connection failed:', err.stack);
-    return;
-  }
-  console.log('Connected to MySQL database.');
-});
-
-module.exports = db;
+module.exports = pool.promise();
