e3f720b16b733e480da2267013c1e9254e8064db
Mohammad Hasnain <mformohammadhasnain@gmail.com>
2025-07-27 18:33:19 +0500

    adding customerRoutes.js

--- src/routes/customerRoutes.js
+++ src/routes/customerRoutes.js
@@ -2,6 +2,24 @@ const express = require('express');
 const router = express.Router();
 const db = require('../config/db');
 
+// POST new customer
+router.post('/', async (req, res) => {
+  const { name, phone, company_name, address } = req.body;
+
+  if (!phone || phone.length !== 11) {
+    return res.status(400).json({ error: "Phone number must be exactly 11 digits." });
+  }
+
+  const query = 'INSERT INTO customer (name, phone, company_name, address) VALUES (?, ?, ?, ?)';
+
+  try {
+    const [result] = await db.query(query, [name, phone, company_name, address]);
+    res.status(201).json({ message: 'Customer inserted', insertedId: result.insertId });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
 // Get all customer
 router.get('/', async (req, res) => {
   const query = 'SELECT * FROM customer';
